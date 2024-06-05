91f1607cfcb6eb281dfe15343d2254f924c1ad0b
Your Name <you@example.com>
2024-06-05 11:22:04 +0700

    Pesan commit Anda

--- index.php
+++ index.php
@@ -5,44 +5,47 @@ $result = $conn->query("SELECT image, title, date, content FROM admin");
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Majalah dinding Smkn 1 Banjar</title>
+    <title>Majalah Dinding SMKN 1 Banjar</title>
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
 </head>
 
 <body>
+    <header class="navbar">
+        <div class="navbar-content">
+            <h3>Majalah Dinding</h3>
+            <nav class="navbar-nav">
+                <a href="login.php">Admin Login</a>
+            </nav>
+        </div>
+    </header>
     <div class="container">
-        <aside class="sidebar">
-            <div class="sidebar-content">
-                <h2>Majalah dinding<br>SMKN 1 BANJAR</h2>
-            </div>
-            <div class="admin-login">
-                <p>
-                    <a href="login.php">admin login</a>
-                </p>
-            </div>
-        </aside>
         <main class="main-content">
             <header class="main-header">
-                <h1>Daily News</h1>
-                <input type="text" placeholder="Search the cards...">
+                <h1>Majalah Dinding SMKN 1 BANJAR</h1>
+                <p>Menampilkan berita terbaru yang ada di sekolah</p>
+                <div class="search-bar">
+                    <input type="text">
+                </div>
             </header>
             <section class="news-cards">
                 <?php while ($row = $result->fetch_assoc()): ?>
                     <div class="card">
-                        <h2><?php echo $row['title']; ?></h2>
-                        <img src="<?php echo $row['image']; ?>" alt="News Image">
-                        <p>Date: <?php echo $row['date']; ?></p>
-                        <p><?php echo $row['content']; ?></p>
+                        <img src="<?php echo $row['image']; ?>" alt="Gambar Berita">
+                        <div class="card-content">
+                            <h2><?php echo $row['title']; ?></h2>
+                            <p class="date"><?php echo $row['date']; ?></p>
+                            <p><?php echo $row['content']; ?></p>
+                        </div>
                     </div>
                 <?php endwhile; ?>
             </section>
         </main>
-        
     </div>
 </body>
 
